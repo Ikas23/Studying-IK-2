cd6cc896318e6e2da6be5e40519ab97d19f43935
Isaac Kazoula <isaac.kazoula@icloud.com>
2024-05-31 10:53:18 +0300

    new change

--- Studying-IK-2/work/ObjectTemplate.cs
+++ Studying-IK-2/work/ObjectTemplate.cs
@@ -45,7 +45,7 @@ namespace Studying_IK_2.work
 
             template1.BoolProperty = true;
             template1.StringProperty = "another string";
-            template1.IntProperty = 20;
+            template1.IntProperty = 200;
 
             Console.WriteLine("Template 1 = IntProperty = {0} , StringProperty = {1}, BoolProperty = {2}",
                 template1.IntProperty, template1.StringProperty, template1.BoolProperty);
